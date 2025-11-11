afaa8eb2c447c9a33168d506c130632b15ed0a3d
FBoucher <fboucher@outlook.com>
2025-11-11 08:21:28 -0500

    Adds extra bash aliases and Hyprland bindings
    Adds a collection of useful bash aliases to the .bashrc file and custom keybindings to the Hyprland configuration. This enhances user experience by providing shortcuts for common tasks and improving window management. Also installs some useful applications via included install scripts.

--- omarchy/post-install.sh
+++ omarchy/post-install.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
 
+MYHOME=$HOME
+
 . ../ghostty/install-ghostty.sh
-. ../dotnet/install-dotnet.sh
+#. ../dotnet/install-dotnet.sh
 . ../brave/install-brave.sh
 . ../rider/install-rider.sh
 . ../keepassxc/install-keepassxc.sh
 . ../gitkraken/install-gitkraken.sh
+. ../yazi/install-yazi.sh
+
+
+if grep -q "# == MY EXTRAS ==" "$MYHOME/.config/hypr/bindings.conf"; then
+    echo "-- Skip bindings EXTRA already present"
+else
+    cat ./extra-bindings.conf >> $MYHOME/.config/hypr/bindings.conf 
+fi
+
+
+
+if grep -q "# == MY EXTRAS ==" "$MYHOME/.bashrc"; then
+    echo "-- Skip Bashrc EXTRA already present"
+else
+    cat ./extra-bashrc >> $MYHOME/.bashrc  
+fi
